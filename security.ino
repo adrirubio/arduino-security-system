e6bf35f5d67b7d83eda0e6fa34053645136c5e47
Adrian Rubio-Punal <adrian.rubio.punal@gmail.com>
2025-08-01 11:45:47 +0200

    Build setup()

--- security.ino
+++ security.ino
@@ -1,2 +1,25 @@
 // c++
 const byte piezoPin = 3;
+const int photoPin = A0;
+const int threshold = 500;
+
+void setup() {
+  // buzzer
+  pinMode(buzzerPin, OUTPUT);
+
+  // photoresistor
+  pinMode(photoPin, INPUT);
+
+  // leds
+  pinMode(4, OUTPUT); // green
+  pinMode(5, OUTPUT); // red
+  pinMode(6, OUTPUT); // yellow
+
+  // pushbutton
+  pinMode(2, INPUT_PULLUP);
+
+  Serial.begin(9600);
+  startupBeep();
+}
+
+void loop() {}
