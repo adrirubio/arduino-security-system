dcd5c04be39e0d5778413f76c6e945a364f1e58e
Adrian Rubio-Punal <adrian.rubio.punal@gmail.com>
2025-08-02 12:41:16 +0200

    Add buzzer sounds

--- firmware/buzzer-tests.ino
+++ firmware/buzzer-tests.ino
@@ -9,3 +9,68 @@ void setup() {
   pinMode(buttonPin, INPUT_PULLUP);
   Serial.begin(9600);
 }
+
+void loop() {
+  bool currentButtonState = digitalRead(buttonPin);
+
+  if (currentButtonState == LOW && lastButtonState == HIGH) {}
+}
+
+// startup beep 1 (original)
+void startupBeep1() {
+  tone(buzzerPin, 1000, 200);
+  delay(250);
+  noTone(buzzerPin);
+}
+
+// startup beep 2 (ascending tones)
+void startupBeep2() {
+  tone(buzzerPin, 800, 150);
+  delay(150);
+  tone(buzzerPin, 1200, 150);
+  delay(150);
+  tone(buzzerPin, 1600, 150);
+  delay(150);
+  noTone(buzzerPin);
+}
+
+// intruder alert 1 (original)
+void intruderAlert1() {
+  unsigned long startTime = millis();
+  while (millis() - startTime < 3000) {
+    tone(buzzerPin, 2000);
+    delay(150);
+    noTone(buzzerPin);
+    delay(50);
+  }
+}
+
+// intruder alert 2 (alternating frequencies)
+void intruderAlert2() {
+  unsigned long startTime = millis();
+  while (millis() - startTime < 3000) {
+    tone(buzzerPin, 1500);
+    delay(200);
+    tone(buzzerPin, 2500);
+    delay(200);
+  }
+  noTone(buzzerPin);
+}
+
+// shutdown beep 1 (original)
+void shutdownBeep1() {
+  tone(buzzerPin, 400, 300);
+  delay(300);
+  noTone(buzzerPin);
+}
+
+// shutdown beep 2 (descending tones)
+void shutdownBeep1() {
+  tone(buzzerPin, 1200, 150);
+  delay(150);
+  tone(buzzerPin, 800, 150);
+  delay(150);
+  tone(buzzerPin, 400, 200);
+  delay(200);
+  noTone(buzzerPin);
+}
